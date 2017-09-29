215dab350c6464106496085a9014e4881d273ed1
Lukas Trümper <lukastruemper@Lukass-MacBook.localdomain>
2017-09-29 11:46:23 +0200

    Example added

---
+++ Example/Example/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Example
+//
+//  Created by Lukas Trümper on 29.09.17.
+//  Copyright © 2017 Lukas Trümper. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Example/Example/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  Example
+//
+//  Created by Lukas Trümper on 29.09.17.
+//  Copyright © 2017 Lukas Trümper. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
